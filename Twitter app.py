88b47009106a79739394b495be0f5d8183c49808
satheesh P <68177021+ssjsmith750@users.noreply.github.com>
2023-02-22 12:14:53 +0530

    Update Twitter app.py

--- Twitter app.py
+++ Twitter app.py
@@ -1,77 +1,88 @@
-import streamlit as st
-import twint
+import snscrape.modules.twitter as sntwitter
 import pandas as pd
-import json
-import botocore
+import streamlit as st
+import datetime
+from pymongo import MongoClient
+import time
 
 
+ # To connect to MONGODB
+ 
+client = MongoClient("mongodb+srv://Itsmesatheesh123:Itsmesatheesh123@cluster0.amauozb.mongodb.net/test") 
+TweetDB = client["Twitter_Database"]  #Creating a New Database in  MongoDB
+tweets_df = pd.DataFrame()
+df = pd.DataFrame()
 
-st.set_page_config(page_title='Twitter scraper',page_icon=':iphone:')
-
-
-st.image('images.png')
-st.subheader("""
-Scrape some Twitter Tweets and Download:
-""")
-
-languages = []
-sentiments = []
-
-with st.form(key='Twitter_form'):
-    search_term = st.text_input('What do you want to search for?')
-    limit = st.slider('How many tweets do you want to get?', 0, 500, step=20)
-    output_csv = st.radio('Save a CSV file?', ['Yes', 'No'])
-    output_Json = st.radio('Save a json file?', ['Yes', 'No'])
-    file_name = st.text_input('Name the CSV file:')
-    file_name = st.text_input('Name the json file:')
-    submit_button = st.form_submit_button(label='Search')
-
-    if submit_button:
-        # configure twint
-        c = twint.Config()
-
-        c.Search = search_term
-        c.Limit = limit
-
-        c.Store_csv = True
-
-        if c.Store_csv:
-            c.Output = f'{file_name}.csv'
-            
-        if c.Store_json:
-            c.Output = f'{file_name}.json'
-
-        twint.run.Search(c)
-
-        data = pd.read_csv(f'{file_name}.csv', usecols=['date', 'tweet'])
-        data1 = open('{file_name}.csv', usecols=['date', 'tweet'])
-        
-
-        for x in data,data1['tweet']:
-            # Get language of the tweet
-            lang = comprehend.detect_dominant_language(Text=x)['Languages'][0]['LanguageCode']
-            languages.append(lang)
-            print(f'Language detected: {lang}')
-        data['languages']=languages
-
-            # Get the sentiment of the tweet
-        for x in range(len(data['tweet'])):
-            try:
-                sent = comprehend.detect_sentiment(
-                                                    Text=data['tweet'][x],
-                                                    LanguageCode= data['languages'][x]
-                                                    )['Sentiment']
-                sentiments.append(sent)
-                print(f'Sentiment detected:{sent}')
-            except botocore.exceptions.ClientError as error:
-                print(f'Error: {error}')
-                sentiments.append(error)
-        data['sentiment']= sentiments
-        st.table(data)
+st.write("Twitter Scrapping")
+option = st.selectbox('How would you like the data to be searched?',('Keyword', 'Hashtag'))
+word = st.text_input('Please enter a '+option, 'Example: #Trending Hashtags')
+start = st.date_input("Select the start date", datetime.date(2022, 1, 1),key='d1')
+end = st.date_input("Select the end date", datetime.date(2023, 1, 1),key='d2')
+tweets = st.slider('How many tweets to scrape', 0, 1000, 5)
+tweets_list = []
 
 
-try:
-    st.download_button(label='Download results', data=convert_df(data), file_name = f'{file_name}.csv', mime='text/csv')
-    st.download_button(label='Download results', data=convert_df(data), file_name = f'{file_name}.json', mime='text/json')
-except:
-    pass
+# Srapping the Data from Twitter
+if word:
+    if option=='Keyword':
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} + since:{start} until:{end}').get_items()):
+            if i>tweets:
+                break
+            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
+    else:
+        for i,tweet in enumerate(sntwitter.TwitterHashtagScraper(f'{word} + since:{start} until:{end}').get_items()):
+            if i>tweets:
+                break            
+            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
+        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
+else:
+    st.warning(option,' cant be empty', icon="⚠️")
+    
+    
+# DOWNLOAD AS CSV
+@st.cache 
+def convert_df(df):    
+    return df.to_csv().encode('utf-8')
+
+if not tweets_df.empty:
+    csv = convert_df(tweets_df)
+    st.download_button(label="Download data as CSV",data=csv,file_name='Twitter_data.csv',mime='text/csv',)
+    
+    
+    # DOWNLOAD AS JSON
+    json_string = tweets_df.to_json(orient ='records')
+    st.download_button(label="Download data as JSON",file_name="Twitter_data.json",mime="application/json",data=json_string,)
+    
+     # UPLOAD DATA TO DATABASE
+    if st.button('Upload Tweets to Database'):
+        coll=word
+        coll=coll.replace(' ','_')+'_Tweets'
+        mycoll=TweetDB[coll]
+        dict=tweets_df.to_dict('records')
+        if dict:
+            mycoll.insert_many(dict) 
+            ts = time.time()
+            mycoll.update_many({}, {"$set": {"KeyWord_or_Hashtag": word+str(ts)}}, upsert=False, array_filters=None)
+            st.success('Successfully uploaded to database', icon="✅")
+            st.balloons()
+        else:
+            st.warning('there are no tweets', icon="⚠️")
+
+    # SHOW TWEETS
+    if st.button('Show Tweets'):
+        st.write(tweets_df)
+
+# SIDEBAR
+with st.sidebar:   
+    st.write('Uploaded Datasets: ')
+    for i in TweetDB.list_collection_names():
+        mycollection=TweetDB[i]
+        #st.write(i, mycollection.count_documents({}))        
+        if st.button(i):            
+            df = pd.DataFrame(list(mycollection.find())) 
+
+# DISPLAY THE DOCUMENTS IN THE SELECTED COLLECTION
+if not df.empty: 
+    st.write( len(df),'Records Found')
+    st.write(df) 
